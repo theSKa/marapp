16828985e4cef9265511ef9fd181da6b6b4e2ee7
Petru Dimulescu <petru.dimulescu@transferwise.com>
2017-12-09 22:27:02 +0000

    All app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Slides} from 'ionic-angular';
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
+@Component({selector: 'page-home', templateUrl: 'home.html'})
 export class HomePage {
+  @ViewChild(Slides)slides : Slides;
 
-  constructor(public navCtrl: NavController) {
+  public images : string[];
 
+  public backgroundColorIndex : number = 0;
+  private backgroundColors : string[] = [
+    "white",
+    "red",
+    "green",
+    "blue",
+    "yellow",
+    "orange",
+    "cyan",
+    "fuchsia",
+    "indigo"
+  ];
+  public backgroundColor : string;
+
+  constructor(public navCtrl : NavController) {
+    this.images = [];
+    for (let i : number = 1; i < 17; i++) {
+      this
+        .images
+        .push(`/assets/imgs/${i}.svg`);
+    }
+    this.backgroundColor = this.backgroundColors[this.backgroundColorIndex];
+  }
+
+  public center() {
+    this.backgroundColorIndex = (this.backgroundColorIndex + 1) % this.backgroundColors.length;
+    this.backgroundColor = this.backgroundColors[this.backgroundColorIndex];
+  }
+
+  public left() {
+    this.moveSlide(-1);
+  }
+
+  public right() {
+    this.moveSlide(1);
+  }
+
+  private moveSlide(step) {
+    let currentIndex = this
+      .slides
+      .getActiveIndex();
+    let nextIndex = currentIndex + step;
+    if (nextIndex > -1 && nextIndex < this.images.length) {
+      this
+        .slides
+        .slideTo(nextIndex, 2000);
+    }
   }
 
 }
